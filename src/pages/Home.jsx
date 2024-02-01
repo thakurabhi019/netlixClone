59cb6de374c904256e8ad20967956394060542aa
Abhi <abhishekloharli19@gmail.com>
2024-02-01 15:28:49 +0530

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function Home(props) {
     return (
         <div>
-            Home
+            Home netflix about netflix and chill 
+
+
+            <p>this is para</p>
+            <br />
+            <h1>this isimy git repo</h1>
             
         </div>
     );
